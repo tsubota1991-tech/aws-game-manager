f62c69e2cc1df5076e432b8060128adc6ace31d2
takuyatsubota1129 <ganbaredekiru@gmail.com>
2025-11-15 00:05:50 +0900

    Initial commit: empty project folder

---
+++ src/main/java/tsubota1991tech/github/io/aws_game_manager/AwsGameManagerApplication.java
@@ -0,0 +1,13 @@
+package tsubota1991tech.github.io.aws_game_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsGameManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsGameManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tsubota1991tech/github/io/aws_game_manager/AwsGameManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package tsubota1991tech.github.io.aws_game_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsGameManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
